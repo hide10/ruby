f7149e2a3a4066110be1442cd2ccc679a1e73ae6
小林 秀人 <kobayashi@msint.com>
2023-02-21 15:18:16 +0900

    タイプミス修正

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -9,7 +9,7 @@ class GateTest < Minitest::Test
         @mikuni = Gate.new(:mikuni)
     end
 
-++    def test_umeda_to_juso
+    def test_umeda_to_juso
         ticket = Ticket.new(160)
         @umeda.enter(ticket)
         assert @juso.exit(ticket)
