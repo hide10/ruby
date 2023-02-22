06e109184dd07c955677bccfde6868ff8af73bd0
小林 秀人 <kobayashi@msint.com>
2023-02-22 10:36:18 +0900

    7.6.5

--- draft.rb
+++ draft.rb
@@ -1314,3 +1314,80 @@ user = User.new('Alice', 20)
 user.name
 user.age = 30
 
+### 7.6.5
+
+class Product
+    attr_reader :name, :price
+
+    def initialize(name, price)
+        @name = name
+        @price = price
+    end
+end
+
+product = Product.new('A Great movie', 1000)
+product.name
+product.price
+
+class DVD < Product
+    attr_reader :running_time
+
+    def initialize(name, price, running_time)
+        @name = name
+        @price = price
+        @running_time = running_time
+    end
+end
+dvd = DVD.new('A great movie', 1000, 120)
+dvd.name
+dvd.price
+dvd.running_time
+
+product = Product.new('A great movie', 1000)
+product.to_s
+
+dvd = DVD.new('An awesome film', 3000, 120)
+dvd.to_s
+
+class Product
+    attr_reader :name, :price
+
+    def initialize(name, price)
+        @name = name
+        @price = price
+    end
+
+    def to_s
+        "name: #{name}, price: #{price}"
+    end
+end
+
+class DVD < Product
+    attr_reader :running_time
+
+    def initialize(name, price, running_time)
+        @name = name
+        @price = price
+        @running_time = running_time
+    end
+
+    def to_s
+        "name: #{name}, price: #{price}, running_time: #{running_time}"
+    end
+end
+dvd.to_s
+
+class DVD < Product
+    attr_reader :running_time
+
+    def initialize(name, price, running_time)
+        @name = name
+        @price = price
+        @running_time = running_time
+    end
+
+    def to_s
+        "#{super}, running_time: #{running_time}"
+    end
+end
+dvd.to_s
