3c2f002af6834c5ad3d23ac2caa76ae12e55e4f9
KOBAYASHI <hide10@gmail.com>
2023-03-16 17:37:46 +0900

    7.9.3 グローバル変数と組み込み変数

--- draft.rb
+++ draft.rb
@@ -1883,3 +1883,37 @@ dvd.upcase_name
 product.name
 Product.name
 DVD.name
+
+
+### 7.9.3 グローバル変数と組み込み変数
+
+# グローバル変数の宣言と値の代入
+$program_name = 'Awesome program'
+
+# グローバル変数に依存するクラス
+class Program
+    def initialize(name)
+        $program_name = name
+    end
+
+    def self.name
+        $program_name
+    end
+
+    def name
+        $program_name
+    end
+end
+
+# $program_nameには既に名前が代入されている
+Program.name
+
+program = Program.new('Super program')
+program.name
+
+# Program.newのタイミングで$program_nameが"Super program"に変更される
+Program.name
+$program_name
+
+# $foobarが未定義であれば(つまり、一度も代入されていなければ)nilが返る
+$foobar
