e83f69b0bf95ae2156c66a97791ef511a36f0898
KOBAYASHI <hide10@gmail.com>
2023-03-16 15:55:45 +0900

    7.9.2 クラス変数

--- draft.rb
+++ draft.rb
@@ -1832,3 +1832,54 @@ dvd.upcase_name
 
 Product.name
 DVD.name
+
+## 7.9.2 クラス変数
+
+class Product
+    @@name = 'Product'
+
+    def self.name
+        @@name
+    end
+
+    def initialize(name)
+        @@name = name
+    end
+
+    def name
+        @@name
+    end
+end
+
+class DVD < Product
+    @@name = 'DVD'
+
+    def self.name
+        @@name
+    end
+
+    def upcase_name
+        @@name.upcase
+    end
+end
+
+# DVDクラスを定義したタイミングで@@nameが"DVD"に変更される
+
+Product.name
+DVD.name
+
+product = Product.new('A great moview')
+product.name
+
+# Product.newのタイミングで@@nameが"A great movie"に変更される
+Product.name
+DVD.name
+
+dvd = DVD.new('An awesome film')
+dvd.name
+dvd.upcase_name
+
+# DVD.newのタイミングで@@nameが"An awesome film"に変更される
+product.name
+Product.name
+DVD.name
