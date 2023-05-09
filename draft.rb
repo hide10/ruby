819e5f92aa3fe8f57c9808e506f66b5ebade3111
KOBAYASHI Hideto <hide10@gmail.com>
2023-05-10 08:16:13 +0900

    ### 9.2.4 例外オブジェクトから情報を取得する

--- draft.rb
+++ draft.rb
@@ -1984,3 +1984,17 @@ t_120 == t_180
 tempos = [Tempo.new(180), Tempo.new(60), Tempo.new(120)]
 #sortメソッドの内部では並び替えの際に<=>演算子が使われる
 tempos.sort
+
+### 9.2.4 例外オブジェクトから情報を取得する
+
+begin
+    1/0
+rescue => e
+    puts "エラークラス:#{e.class}"
+    puts "エラーメッセージ:#{e.message}"
+    puts "バックトレース ----"
+    puts e.backtrace
+    puts "----"
+end
+
+### 9.2.5 クラスをしてして補足する例外を限定する
