f094534aedb983b2f435f0acd8fbc6537fc286c1
KOBAYASHI Hideto <hide10@gmail.com>
2023-06-03 08:21:35 +0900

    ログフォーマッタ作成準備

---
+++ test/log_formatter_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative '../lib/log_formatter'
+
+class LogFormatterTest < Minitest::Test
+    def test_format_log
+        # とりあえずプログラムを動かすためのコードを書く
+        LogFormatter.format_log
+    end
+end
