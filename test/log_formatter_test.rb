e30444292403002cedb193afe1830217af3d740c
KOBAYASHI Hideto <hide10@gmail.com>
2023-06-03 08:30:43 +0900

    "OK"のログに対応

--- test/log_formatter_test.rb
+++ test/log_formatter_test.rb
@@ -3,7 +3,8 @@ require_relative '../lib/log_formatter'
 
 class LogFormatterTest < Minitest::Test
     def test_format_log
-        # とりあえずプログラムを動かすためのコードを書く
-        LogFormatter.format_log
+        text = LogFormatter.format_log
+        lines = text.lines(chomp:true)
+        assert_equal '[OK] request_id=1,path=/products/1',lines[0]
     end
 end
